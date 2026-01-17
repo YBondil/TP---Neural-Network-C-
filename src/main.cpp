9e39e9242310f2a0d999e3fbb1aca4e6692d5169
Yoan Bondil <yoan.bondil@etu.minesparis.psl.eu>
2026-01-17 21:33:50 +0100

    main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,16 @@
 #include "csv_reader.h"
 
 int main(){
-    std::vector<CSVReader::digit> digits = CSVReader::readDigits("data/Mnist/test.csv") ;
-    digits[99].visualize();
+    std::vector<CSVReader::digit> digits_dev = CSVReader::readDigits("data/Mnist/test.csv", 100) ;
+    digits_dev[99].visualize();
+    
+
+
+    int a = 3;
+    int atab [4] = {1,2,3,4} ; 
+    for (int i = 0; i<4; i++){
+        std::cout<<*(atab+i) << std::endl;
+}
+
     return 0;
 }
