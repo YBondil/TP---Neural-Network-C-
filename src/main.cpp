820570d5be442d4cf38973530171c063e8507d3f
Yoan Bondil <yoan.bondil@etu.minesparis.psl.eu>
2026-01-18 20:45:44 +0100

    learning test

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,32 @@
 #include "csv_reader.h"
 #include "neural_net.h"
 
-int main(){
-    int layers[] = {784, 128, 10};
-    NeuralNetwork net(3, layers, 2);
+int main() {
+    int layers_config[] = {784, 128, 10};
+    NeuralNetwork net(3, layers_config, 4); 
+    net.initialize_parameters();
+
     
-    Matrix batch = CSVReader::readAsMatrix("data/Mnist/train.csv", ',', 6);
+    Matrix raw_data = CSVReader::readAsMatrix("data/Mnist/train.csv", ',', 1000);
     
-    // Extract labels (first column) from the batch
-    Matrix labels = batch.sub_col(0);
-    Matrix batch = CSVReader::readAsMatrix("data/Mnist/train.csv", ',', 6);
-    // Normalize: scale pixel values from [0, 255] to [0, 1]
-    batch = batch * (1.0f / 255.0f);
-    Matrix y = labels.transposed().to_label_matrix();
     
-    std::cout << "Labels:" << std::endl;
-    labels.transposed().print();
-    std::cout << "One-hot encoded:" << std::endl;
-    y.print();
-    
+    net.learn(raw_data, 0.1f, 5, "model.csv");
+
+    while(true){
+    int test;
+        std::cout << "quel test ?" ;
+        std::cin >> test ;
+        
+        Matrix test_row = raw_data.sub_row(test); 
+        
+        
+        Matrix image(784, 1);
+        for (int j = 0; j < 784; j++) {
+            image(j, 0) = test_row(0, j + 1) / 255.0f; 
+        }
+        
+        
+        net.prediction(image);
+    }    
     return 0;
 }
